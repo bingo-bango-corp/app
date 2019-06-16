724caa170571ebbf96ae0da39f1900632e050a67
Jason Efstathiou <jason.efst@gmail.com>
2019-06-16 18:20:07 +0200

    only load service worker in prod

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -3,32 +3,34 @@ import firebase from 'firebase/app'
 import '@firebase/messaging'
 
 export default () => {
-  const messaging = firebase.messaging()
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB',
-      )
-    },
-    registered(registration) {
-      messaging.useServiceWorker(registration)
-      console.log('Service worker has been registered.')
-    },
-    cached() {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound() {
-      console.log('New content is downloading.')
-    },
-    updated() {
-      (window as any).updateAvailable = true
-    },
-    offline() {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error(error) {
-      console.error('Error during service worker registration:', error)
-    },
-  })
+  if (process.env.NODE_ENV === 'production') {
+    const messaging = firebase.messaging()
+    register(`${process.env.BASE_URL}service-worker.js`, {
+      ready() {
+        console.log(
+          'App is being served from cache by a service worker.\n' +
+          'For more details, visit https://goo.gl/AFskqB',
+        )
+      },
+      registered(registration) {
+        messaging.useServiceWorker(registration)
+        console.log('Service worker has been registered.')
+      },
+      cached() {
+        console.log('Content has been cached for offline use.')
+      },
+      updatefound() {
+        console.log('New content is downloading.')
+      },
+      updated() {
+        (window as any).updateAvailable = true
+      },
+      offline() {
+        console.log('No internet connection found. App is running in offline mode.')
+      },
+      error(error) {
+        console.error('Error during service worker registration:', error)
+      },
+    })
+  }
 }
