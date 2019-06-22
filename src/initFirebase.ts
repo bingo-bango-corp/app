94ad965bd92ddeb8776b01ad69c0dfdb1c6d3a01
Jason Efstathiou <jason.efst@gmail.com>
2019-06-23 01:38:45 +0200

    add firebase performance monitoring

--- src/initFirebase.ts
+++ src/initFirebase.ts
@@ -1,12 +1,14 @@
 import * as Firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/messaging'
+import 'firebase/performance'
 
 import firebaseConfig from './config/firebase'
 
 export default () => {
   Firebase.initializeApp(firebaseConfig)
   Firebase.messaging().usePublicVapidKey(process.env.VUE_APP_FIREBASE_MESSAGING_KEY)
+  Firebase.performance()
   if (window.matchMedia('(display-mode: standalone)').matches) {
     return new Promise<void>((resolve, reject) => {
       Firebase.firestore().enablePersistence()
