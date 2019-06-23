215d52cb2a8b0d841984199627c2d2ec49762c93
Jason Efstathiou <jason.efst@gmail.com>
2019-06-23 19:49:57 +0200

    rudimentary open / settled job view in get-things

--- src/views/getThings/getThings.vue
+++ src/views/getThings/getThings.vue
@@ -3,20 +3,42 @@
     <BingoButton class="newRequestButton" @clicked="handleNewClick">{{ $t('getThings.newRequest') }}</BingoButton>
     <div class="yourJobs">
       <JobListView :loading="loading">
-        <div class="job" v-for="(job, key) in myJobs" :key="key">
-          <JobCard class="jobCard"
-            :title="job.thing"
-            :jobId="key"
-            :description="job.description"
-            :actions="actions"
-            :tip="{
-              cents: job.tip.cents,
-              currency: job.tip.currency
-            }"
-            :collapsed="false"
-            :elevated="false"
-            :locale="$i18n.locale"
-          />
+        <div class="open" v-if="openJobs.length > 0">
+          <h3>Current Jobs</h3>
+          <div class="job" v-for="(job) in openJobs" :key="job.id">
+            <JobCard class="jobCard"
+              :title="job.thing"
+              :jobId="job.id"
+              :description="job.description"
+              :actions="actionsForJobState(job.state)"
+              :tip="{
+                cents: job.tip.cents,
+                currency: job.tip.currency
+              }"
+              :collapsed="false"
+              :locale="$i18n.locale"
+              @click.native="handleCardClick(job.id)"
+            />
+          </div>
+        </div>
+        <div class="settled" v-if="settledJobs.length > 0">
+          <h3>Past Jobs</h3>
+          <div class="job" v-for="(job) in settledJobs" :key="job.id">
+            <JobCard class="jobCard"
+              :title="job.thing"
+              :jobId="job.id"
+              :description="job.description"
+              :actions="actionsForJobState(job.state)"
+              :tip="{
+                cents: job.tip.cents,
+                currency: job.tip.currency
+              }"
+              :collapsed="false"
+              :elevated="false"
+              :locale="$i18n.locale"
+              @click.native="handleCardClick(job.id)"
+            />
+          </div>
         </div>
       </JobListView>
     </div>
@@ -25,10 +47,11 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-import { getOwnJobs, JobList } from '@/helpers/jobs'
+import { getOwnJobs, JobList, cancelJob } from '@/helpers/jobs'
 import JobListView from '@/components/JobListView'
 import router from '@/router'
 import { BingoButton, JobCard } from 'simsalabim-design'
+import { Job, OPEN_STATES, SETTLED_STATES, State } from '../../store/models/job'
 
 @Component({
   components: {
@@ -38,10 +61,10 @@ import { BingoButton, JobCard } from 'simsalabim-design'
   }
 })
 export default class getThings extends Vue {
-  myJobs: JobList = {}
+  myJobs: Job[] = []
   loading: boolean = true
 
-  actions = [
+  assignedJobActions = [
     {
       title: '💬 Go to chat',
       backgroundColor: 'var(--secondary)',
@@ -50,16 +73,80 @@ export default class getThings extends Vue {
         router.push(`/get-things/${event.meta.jobId}`)
       }
     },
+    {
+      title: '🚫 Cancel job',
+      backgroundColor: '#EB5757',
+      onClick: (event: any) => {
+        event.event.stopPropagation()
+        this.cancelJob(event)
+      }
+    },
   ]
 
-  handleNewClick() {
+  unassingedJobActions = [
+    {
+      title: '🚫 Cancel job',
+      backgroundColor: '#EB5757',
+      onClick: (event: any) => {
+        event.event.stopPropagation()
+        this.cancelJob(event)
+      }
+    },
+  ]
+
+  actionsForJobState(state: State) {
+    switch (state) {
+      case 'unassigned':
+        return this.unassingedJobActions
+      case 'assigned':
+        return this.assignedJobActions
+      default:
+        return null
+    } 
+  }
+
+  handleNewClick(): void {
     this.$router.push('get-things/new')
   }
 
-  async mounted() {
+  handleCardClick(id: string): void {
+    this.$router.push(`/get-things/${id}`)
+  }
+
+  get openJobs(): Job[] {
+    return this.myJobs.filter((j: Job) => {
+      return OPEN_STATES.includes(j.state)
+    })
+  }
+
+  get settledJobs(): Job[] {
+    return this.myJobs.filter((j: Job) => {
+      return SETTLED_STATES.includes(j.state)
+    })
+  }
+
+  jobArrayFromJobList(jobList: JobList): Array<Job> {
+    return Object.keys(jobList).map((k) => {
+      return {
+        id: k,
+        ...jobList[k]
+      }
+    })
+  }
+
+  async cancelJob(e: any): Promise<void> {
+    await cancelJob(e.meta.jobId, this.$store.getters.uid)
+    this.updateJobs()
+  }
+
+  async updateJobs(): Promise<void> {
     this.loading = true
-    this.myJobs = await getOwnJobs(this.$store.getters.uid)
+    this.myJobs = this.jobArrayFromJobList(await getOwnJobs(this.$store.getters.uid))
     this.loading = false
+  }
+
+  async mounted(): Promise<void> {
+    await this.updateJobs()
   }
 } 
 </script>
