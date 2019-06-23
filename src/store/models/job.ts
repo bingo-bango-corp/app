5655c11ac6fb1ee657af3f5a17152a2f2b683242
Jason Efstathiou <jason.efst@gmail.com>
2019-06-23 19:48:34 +0200

    fix Jobs type

--- src/store/models/job.ts
+++ src/store/models/job.ts
@@ -2,23 +2,8 @@ import { PublicProfileRef } from '@/store/models/profile'
 import { GeoPoint } from '@firebase/firestore-types'
 
 export type Job = {
-  state: 'assigned' | 'delivered' | 'cancelled'
-  assignee: PublicProfileRef
-  owner: PublicProfileRef
-  description: string
-  id?: string
-  point: {
-    geohash: string
-    geopoint: GeoPoint
-  }
-  thing: string
-  timestamp: any
-  tip: {
-    cents: number
-    currency: string
-  }
-} | {
-  state: 'unassigned' | 'cancelled'
+  state: State
+  assignee?: PublicProfileRef
   owner: PublicProfileRef
   description: string
   id?: string
@@ -33,3 +18,35 @@ export type Job = {
     currency: string
   }
 }
+
+export const STATE_CONSTANTS = {
+  UNASSIGNED: 'unassigned',
+  ASSIGNED: 'assigned',
+  DELIVERED: 'delivered',
+  CANCELLED: 'cancelled',
+  LOST: 'lost'
+}
+
+// All states that may transition to another.
+export const OPEN_STATES = [
+  STATE_CONSTANTS.UNASSIGNED,
+  STATE_CONSTANTS.ASSIGNED,
+]
+
+// Terminal states that will never change to something else.
+export const SETTLED_STATES = [
+  STATE_CONSTANTS.DELIVERED,
+  STATE_CONSTANTS.CANCELLED,
+  STATE_CONSTANTS.LOST,
+]
+
+export type OpenState =
+  'unassigned' |
+  'assigned'
+
+export type SettledState =
+  'delivered' |
+  'cancelled' |
+  'lost'
+
+export type State = OpenState | SettledState
